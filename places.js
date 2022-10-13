a6596fcf4c19c4e1a157f568707b57f2d90cda1c
Elsa Katrín <elsa.katrin@gmail.com>
2022-10-13 16:13:39 +0000

    added to places.js to make places appear on webpage in the order that we want (time and date)

--- places.js
+++ places.js
@@ -1,2 +1,89 @@
-let list = document.getElementById("bars-container");
-list.innerHTML+= div 
+let nowlist = document.getElementById("now");
+
+places.forEach(element => {
+ 
+nowlist.innerHTML+= `
+  <div id="one">
+                <img class="images" id="cocktailbar" src="images/bars/cocktailbar.JPG" alt="cocktailbar">
+                <div id="info">
+                <h3>
+                    ${element.bar_name}
+                </h3>
+                <p>
+                ${element.address}
+                </p>
+                <div id="clock">
+                    <p>
+                    ${element.happyhours}
+                    </p>
+                </div>
+                </div>
+                
+            </div>
+`
+
+});
+
+
+
+
+let barlist = document.getElementById("bars-container");
+
+places.filter (place=>place.type === "bars" )
+
+.forEach(element => {
+ 
+barlist.innerHTML+= `
+  <div id="one">
+                <img class="images" id="cocktailbar" src="images/bars/cocktailbar.JPG" alt="cocktailbar">
+                <div id="info">
+                <h3>
+                    ${element.bar_name}
+                </h3>
+                <p>
+                ${element.address}
+                </p>
+                <div id="clock">
+                    <p>
+                    ${element.happyhours}
+                    </p>
+                </div>
+                </div>
+                
+            </div>
+`
+    
+});
+
+let resturantlist = document.getElementById("resturant-container");
+
+places.filter (place=>place.type === "resturants" )
+
+.forEach(element => {
+ 
+resturantlist.innerHTML+= `
+  <div id="one">
+                <img class="images" id="cocktailbar" src="images/bars/cocktailbar.JPG" alt="cocktailbar">
+                <div id="info">
+                <h3>
+                    ${element.bar_name}
+                </h3>
+                <p>
+                ${element.address}
+                </p>
+                <div id="clock">
+                    <p>
+                    ${element.happyhours}
+                    </p>
+                </div>
+                </div>
+                
+            </div>
+`
+    
+});
+
+
+
+
+           

ef8f764e1040275111f257ff9bdcdb96b84fd7a1
Lydia <lydiadoula@gmail.com>
2022-10-24 13:09:41 +0100

    contact us popup

--- popup.js
+++ popup.js
@@ -1,4 +1,9 @@
 let popup = document.getElementById("popup");
+function noRefresh(e){ //e=event
+    e.preventDefault() //kalla á prevent default
+}
+const formElement = document.querySelector("form") //ná í form
+formElement.addEventListener("submit",noRefresh) //hlusta á submit, kalla á no refresh
 
 function openPopup(){
     popup.classList.add("open-popup");
